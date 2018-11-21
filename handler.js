539ed9d457e67a7cc9a239832504a94ffcfd294f
qjh <qjh886@qq.com>
2018-11-21 11:24:23 +0800

    提交数据

--- handler.js
+++ handler.js
@@ -1,6 +1,11 @@
 var path =require('path');
 var mysql  = require('mysql');
 var dbConfig =require('./DBConfig.js');
+
+
+
+// app.use(bodyparser.urlencoded({extende:true}));
+// app.use(bodyparser.json())
 //连接数据库
 var connection = mysql.createConnection(dbConfig.mysql);
   connection.connect();
@@ -10,19 +15,43 @@ var connection = mysql.createConnection(dbConfig.mysql);
 module.exports.index =function(req,res){
    // res.send('index');
    //   要使用模板引擎  不使用  res.sendFile 
-   var sql = "SELECT * FROM user";
-   
-   connection.query(sql, function (error, results, fields) {
-     if (error) throw error;
-     console.log('The solution is: ', results);
-     res.send(results);
-   }); 
+//    var sql = "SELECT * FROM user";
+//    connection.query(sql, function (error, results, fields) {
+//      if (error) throw error;
+//      console.log('The solution is: ', results);
+//      res.send(results);
+//    }); 
 
-    //res.sendFile(path.join(__dirname,'public','html','index.html'));
+    res.sendFile(path.join(__dirname,'public','html','index.html'));
     // 使用 res.render    但是默认的不能使用 需要配置一个模板引擎才能使用 ejs
     //res.render(path.join(__dirname,'public','html','index.html'));
 };
 module.exports.list =function(req,res){
     //res.send('list');
     res.sendFile(path.join(__dirname,'public','html','list.html'));
+};
+module.exports.result =function(req,res){
+  
+        var   username = req.body.username;
+        var   password = req.body.password;
+        var   open_id = req.body.open_id;
+        // 输出 JSON 格式
+        var response = [
+            username,
+            password,
+            open_id,
+            ];
+           
+        var sql = " INSERT INTO user(username,password,open_id) VALUES(?,?,?)";
+        connection.query(sql, response ,function (error, results, fields) {
+                if (error) throw error;
+                    console.log('The solution is: ', results);
+                   res.send('请求成功');
+                }); 
+        
+     
+        
+
+    //res.send('list');
+   // res.sendFile(path.join(__dirname,'public','html','list.html'));
 };
--- router.js
+++ router.js
@@ -1,10 +1,15 @@
 var express =require('express');
-var handler =require('./handler.js');
+var bodyParser = require('body-parser'); //用于处理 post请求
 var path =require('path');
+
+var handler =require('./handler.js');
 var router =express.Router();
+   // 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get('/',handler.index);
 router.get('/list',handler.list);
+router.post('/result',urlencodedParser,handler.result);
 //实现对静态资源的托管
 router.use('/css',express.static(path.join(__dirname,'css')));
 module.exports = router;
